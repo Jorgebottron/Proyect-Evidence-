270fe8c6566ca424e924a8000739034ba96edb47
Diego Quezada <a01710024@tec.mx>
2025-03-21 15:12:44 -0600

    Increase the speed of the ghosts by 60%

--- pacman.py
+++ pacman.py
@@ -22,10 +22,10 @@ writer = turtle.Turtle(visible=False)
 aim = vector(5, 0)
 pacman = vector(-40, -80)
 ghosts = [
-    [vector(-180, 160), vector(5, 0)],
-    [vector(-180, -160), vector(0, 5)],
-    [vector(100, 160), vector(0, -5)],
-    [vector(100, -160), vector(-5, 0)],
+    [vector(-180, 160), vector(8, 0)],
+    [vector(-180, -160), vector(0, 8)],
+    [vector(100, 160), vector(0, -8)],
+    [vector(100, -160), vector(-8, 0)],
 ]
 tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
